5736cda4ee929e7efc0f91cc4f8c3ede656abc14
kruparaoKapil <kruparao.g@kapilit.com>
2025-10-13 00:51:14 +0530

    added triggers and socket for devices

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const { express} = require("./deps");
+const { express } = require("./deps");
 const { Server } = require("socket.io");
 const http = require("http");
 require('dotenv').config();
@@ -10,7 +10,7 @@ const port = process.env.PORT || 4000;
 const webhook = require("./weebhook");
 // Middleware
 app.use(cors());
-app.use("/superadmin/payments/webhooks",webhook)
+app.use("/superadmin/payments/webhooks", webhook)
 app.use(express.json())
 const rootRouterAdvertiser = require("./advertiserApis");
 const order = require("./order");
@@ -20,6 +20,7 @@ const superAdminApis = require("./superadminApis");
 const superadminAnalyticsApis = require("./superadminAnalyticsApis");
 const superadminPayments = require("./superadminPayments");
 const apisForTv = require("./apisForTvApp");
+const { log } = require("console");
 app.use("/superadmin", superAdminApis);
 app.use("/superadmin", superadminAnalyticsApis);
 app.use("/superadmin/payments", superadminPayments);
@@ -27,7 +28,7 @@ app.use("/advertiser", rootRouterAdvertiser); // e.g. GET /
 app.use("/admin", adminApis); // e.g. GET /
 app.use("/api", apiRoutes); // e.g. GET /api/users
 app.use("/tvApp", apisForTv); // e.g. GET /api/users
-app.use("/advertiser/payments",order)
+app.use("/advertiser/payments", order)
 
 
 const server = http.createServer(app);
@@ -38,23 +39,35 @@ const io = new Server(server, {
   },
 });
 
-client.query("LISTEN pause_all_ads_channel");
+client.on("notification", async (msg) => {
+  if (msg.channel === "device_status_channel") {
+     console.log(msg.payload);
+    if (msg.payload.change_type === "status") {
+      io.to(msg.payload.device_id).emit("device_status_updated", msg.payload);
+    } else if (msg.payload.change_type === "emergency") {
+      io.to(msg.payload.device_id).emit("emergency_mode_updated", msg.payload);
+    }
+  } else if (msg.channel === "client_subscription_channel") {
+    console.log(msg.channel);
+    
+    
+    const { client_id, subscription_status } =msg. payload;
+    console.log(msg. payload);
 
-client.on("notification", (msg) => {
-  const payload = JSON.parse(msg.payload);
-  console.log("DB change:", payload);
-
-  // Send to connected clients
-  io.emit("pauseAllAdsUpdate", payload);
+    // fetch all devices under this client
+    const devicesRes = await client.query(
+      `SELECT id FROM devices WHERE client_id = $1`,
+      [client_id]
+    );
+    // broadcast to all devices in that client's group
+    for (const row of devicesRes.rows) {
+      io.to(row.id).emit("subscription_status_updated", payload);
+    }
+  }
 });
 
-io.on("connection", (socket) => {
-  console.log("Client connected:", socket.id);
-  
-  socket.on("disconnect", () => {
-    console.log("Client disconnected:", socket.id);
-  });
-});
+client.query("LISTEN device_status_channel");
+client.query("LISTEN client_subscription_channel");
 
 server.listen(port, "0.0.0.0", () => {
   console.log(`🚀 Server running at: http://localhost:${port}`);
